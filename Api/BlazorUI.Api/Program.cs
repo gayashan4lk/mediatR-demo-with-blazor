a47cf57884ab91015d2f8e319b68190222eac99a
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-04-04 18:59:57 +0530

    create asp.net core project and add mediatR

---
+++ Api/BlazorUI.Api/Program.cs
@@ -0,0 +1,29 @@
+using Core;
+using Core.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IDataAcess, DemoDataAcess>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<DemoLibraryMediatREntryPoint>());
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
